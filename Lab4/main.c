acdc1dcbcb67a16361f2c370e26e69b82195c32d
nicpadilla <hotelmariofan@gmail.com>
2017-02-20 19:59:09 -0600

    completed Fileread fxn

--- Lab4/main.c
+++ Lab4/main.c
@@ -6,12 +6,15 @@
 
 
 int* FileRead(char*);
+int* Offset(char*, int); //offset factor
+int* Scale(char*, int); //scale factor
+
 
 int main(int argc, char *argv[])
 {
-	int i = 1, count = 0, filenum = 0;
+	int i = 1, count = 0, filenum = 0, n = 0, size;
         double offset = 0, scale = 0;
-        char filename[50];
+        char* filename = malloc(sizeof(char)*15); //given length of filename
         int* data = NULL;
         char* error = "Error, you must enter '-n' in the command line argument followed by the file number.\n"
                         "You may then use any of the following arguments after that:\n"
@@ -23,45 +26,64 @@ int main(int argc, char *argv[])
             if((argv[i][0] == '-') && (argv[i][1] == 'n')) {
                 if(isdigit(argv[i+1][0])) {
                     filenum = atoi(argv[i+1]);
-                    sprintf(filename, "Raw_data_%.2d", filenum);
+                    sprintf(filename, "Raw_data_%.2d.txt", filenum);
                     data = FileRead(filename);
                 }
-                else {
-                    printf("%s", error);
-                }
+                else printf("%s", error);
             }
             else if((argv[i][0] == '-') && (argv[i][1] == 'o')) {
-                (isdigit(argv[i+1][0])) ? offset = strtod(argv[i+1],NULL) : printf("%s", error); //checks if first character is a digit
+                if(isdigit(argv[i+1][0])) {
+                    offset = strtod(argv[i+1],NULL);
+                    data = FileRead(filename);
+                }
+                    else printf("%s", error); //checks if first character is a digit
             }
             else if((argv[i][0] == '-') && (argv[i][1] == 's')) {
-                (isdigit(argv[i+1][0])) ? scale = strtod(argv[i+1],NULL) : printf("%s", error);
+                if(isdigit(argv[i+1][0])) {
+                        scale = strtod(argv[i+1],NULL);
+                        data = FileRead(filename);
+                }
+                        else printf("%s", error);
             }
             else if((argv[i][0] == '-') && (argv[i][1] == 'r')) {
-                
+                //check for next char
+                data = FileRead(filename);
+                //else printf("%s", error);
             }
             else if((argv[i][0] == '-') && (argv[i][1] == 'h')) {
                 printf("%s", error);
             }
             i++;
         }
+        /*for(size = data[1]+2; n < size; n++) {
+            printf("%d ", data[n]);
+        }*/
 	return 0;
 }
 
 int* FileRead(char* filename)
 {
     FILE* fp = fopen(filename, "r");
-    int i, length;
+    int i, size, temp, max;
     if(fp == NULL) {
         printf("The file was not opened!");
         return 0;
     }
-    fseek(fp,0,SEEK_END);
-    length = (int)ftell(fp)/sizeof(int);
-    int* data = (int*)malloc(sizeof(int)*length); //allocates correct space
+    fscanf(fp, "%d", &size);
+    size = size + 2;
+    int* data = (int*)malloc(sizeof(int)*size); //allocates correct space
     rewind(fp);
-    for(int i = 0; i < length; i++) {
-        fscanf(fp, "%d", data[i]);
+    for(i = 0; i < size; i++) {
+        fscanf(fp, "%d", &data[i]);
     }
     fclose(fp);
     return data;
+}
+
+int* Offset(char*, int num) {
+    
+}
+
+int* Scale(char*, int factor) {
+    
 }
